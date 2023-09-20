f50ebe22c7bd6a51952cf1ac1580b4fce6b3588b
Tureckij <cararama18@gmail.com>
2023-09-20 20:30:00 +0500

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,97 @@
+
+public class Main {
+    // Лаб 1
+    // Зад 1
+    /*public static void main(String[] args) {
+        int x = 1;
+        System.out.println("Перед началом цикла");
+        while (x < 4) {
+            System.out.println("Внутра цикла");
+            System.out.println("Значение x равно" + x);
+            x = x + 1;
+        }
+        System.out.println("После окончания цикла");
+    }*/
+    // Зад 2
+    /*public static void main (String[] args) {
+        int x = 3;
+        if (x == 3) {
+            System.out.println("x должен равняться 3");
+        }
+        System.out.println("Эта строка выполняется в любом случае");
+    }*/
+    //Зад 3
+    /*public static void main (String [] args) {
+        int x = 2;
+        if (x == 3) {
+            System.out.println("x должен равняться 3");
+        } else {
+            System.out.println("x не равен  3");
+        }
+        System.out.println("Эта строка выполняестя в любом случае");
+    }*/
+    //Зад 4
+    /*public static void main(String[] args){
+        int x = 1;
+        while (x < 3){
+            System.out.print("Doo");
+            System.out.print("Bee");
+            x = x + 1;
+        }
+        if (x == 3){
+            System.out.print("Do");
+        }
+    }*/
+    //Зад 5
+/*public static void main (String [] args){
+
+    int x = 3;
+    while (x > 0) {
+        if (x > 2) {
+            System.out.print("a");
+        }
+        if (x == 2) {
+            System.out.print("b c");
+
+        }
+        x = x - 1;
+        System.out.print("-");
+        if (x == 1) {
+            System.out.print("d");
+            x = x - 1;
+        }
+
+    }
+}*/
+    //Зад 6
+    /*public static void main(String[] args) {
+        int x = 1;
+        while (x < 10) {
+            if (x > 3) {
+                System.out.println("большой икс");
+            }
+            x = x + 1;
+        }
+    }*/
+    //Зад 7
+    /*public static void main(String [] args) {
+        int x = 5;
+        while (x > 1) {
+            x = x - 1;
+            if (x < 3) {
+                System.out.println("маленький икс");
+            }
+        }
+    }*/
+    public static void main(String[] args) {
+        int x = 5;
+        while (x > 1) {
+            x = x - 1;
+            if (x < 3) {
+                System.out.println("маленький икс");
+            }
+
+        }
+    }
+
+}
---
+++ src/PollPuzzleOne.java
@@ -0,0 +1,26 @@
+public class PollPuzzleOne {
+    public static void main (String [] args){
+        int x = 0;
+        while (x < 4){
+            System.out.print("a ");
+            if (x < 1) {
+                System.out.print("noise");
+            }
+            x = x + 2;
+            if (x > 1) {
+                System.out.println("");
+            x = x - 2;
+            }
+            if (x == 1){
+                System.out.print("annoys");
+            }
+            if (x > 3){
+                System.out.print(" oyster");
+            }
+            System.out.println("");
+            x = x + 1;
+
+        }
+
+    }
+}
